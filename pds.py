eaf32a7d00e50c0a4ca703e306cdd6ef9e3ae23a
omer <omer.mizrahi@hpe.com>
2023-07-30 22:05:18 +0300

    extract morph info

--- pds.py
+++ pds.py
@@ -34,24 +34,6 @@ def get_sentence_syntactic_info(df_row, root):
     return None
 
 
-excel_file_path = 'tagged_verbs.xlsx'
-sheet_name = 'Tanach verbs'
-df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
-
-
-grouped = df.groupby('source')
-
-# Step 2: Access individual groups as separate DataFrames
-list_of_dataframes = [group for _, group in grouped]
-group_names = [name for name, _ in grouped]
-
-df1 = list_of_dataframes[0]
-row = df1.iloc[0]
-path_to_file = os.path.join("tanach", "Exodus.xml")
-tree = ET.parse(path_to_file)
-root = tree.getroot()
-words = get_sentence_words(row, root)
-info = get_sentence_syntactic_info(row, root)
 def baseform_filter(m):
     pattern = r"^ #BASEFORM"
     if m is not None:
@@ -62,7 +44,39 @@ def baseform_filter(m):
     return False
 
 
-baseform_words = list(filter(baseform_filter, words))
-print(len(baseform_words))
-print(len(info))
+def extract_BASEFORM_X_Y(input_string):
+    # Define the regex pattern to match "BASEFORM_" followed by X and Y
+    pattern = r"BASEFORM_([A-Z]+)_([A-Z]+)"
+    matches = re.findall(pattern, input_string)
+    extracted_values = {}
+    for match in matches:
+        x_value, y_value = match
+        extracted_values[x_value] = y_value
 
+    return extracted_values
+
+
+# read the excel file
+excel_file_path = 'tagged_verbs.xlsx'
+sheet_name = 'Tanach verbs'
+df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+
+# group the dataframe by source
+grouped = df.groupby('source')
+list_of_dataframes = [group for _, group in grouped]
+sources = [name for name, _ in grouped]
+
+
+for df,source in zip(list_of_dataframes, sources):
+    for index, row in df.iterrows():
+        path_to_file = os.path.join("tanach", source)
+        tree = ET.parse(path_to_file)
+        root = tree.getroot()
+        words = get_sentence_words(row, root)
+        info = get_sentence_syntactic_info(row, root)
+        baseform_words = list(filter(baseform_filter, words))
+        print(len(baseform_words))
+        print(len(info))
+        for word in baseform_words:
+            w = extract_BASEFORM_X_Y(baseform_words[0].get('ana'))
+            w['phraseId'] = word.get('phraseId')
