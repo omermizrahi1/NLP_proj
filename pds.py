8a8531b1d18b7bb463aee444fc3eac6bac5c4abc
Oren <orenpartovi@gmail.com>
2023-07-31 16:48:19 +0300

    window

--- pds.py
+++ pds.py
@@ -69,7 +69,7 @@ def extract_BASEFORM_X_Y(input_string):
 
 def dict_morpholical(word):
     w = extract_BASEFORM_X_Y(word.get('ana'))
-    w['phraseId'] = word.get('phraseId')
+    w['phrase id'] = word.get('phraseId')
     w['text'] = word.text
     return w
 
@@ -78,7 +78,7 @@ def dict_morpholical(word):
 def dict_word(word, info, sentence_id, target_verb):
     w = dict_morpholical(word)
     phrase_dict = dict_phrases(info)
-    phrase_id = w['phraseId']
+    phrase_id = w['phrase id']
     w['function'] = phrase_dict[phrase_id]
     w['sentence id'] = sentence_id
     w['target verb'] = target_verb
@@ -89,19 +89,19 @@ def dict_word(word, info, sentence_id, target_verb):
 # read the excel file
 excel_file_path = 'verbs.xlsx'
 sheet_name = 'Tanach verbs'
-df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
-
+verbs_df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+# print(f'verbs data frame:\n {verbs_df.to_dict()}')
 # group the dataframe by source
-grouped = df.groupby('source')
+grouped = verbs_df.groupby('source')
 list_of_dataframes = [group for _, group in grouped]
 sources = [name for name, _ in grouped]
 
 df_list = []
-for df,source in zip(list_of_dataframes, sources):
+for verbs_df,source in zip(list_of_dataframes, sources):
     path_to_file = os.path.join("tanach", source)
     tree = ET.parse(path_to_file)
     root = tree.getroot()
-    for index, row in df.iterrows():
+    for index, row in verbs_df.iterrows():
         words = get_sentence_words(row, root)
         info = get_sentence_syntactic_info(row, root)
         baseform_words = list(filter(baseform_filter, words))
@@ -111,3 +111,37 @@ for df,source in zip(list_of_dataframes, sources):
             df_list.append(pd.DataFrame(w, index=[0]))
 combined_df = pd.concat([df for df in df_list], ignore_index=True)
 combined_df.to_excel('senteces.xlsx', index=False)
+
+excel_file_path = 'verbs.xlsx'
+sheet_name = 'Tanach verbs'
+verbs_df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+def get_window(words, index, window_size):
+    start = max(0, index - window_size)
+    end = min(len(words), index + window_size + 1)
+    return words[start:end]
+
+# Loop through the rows of the verbs DataFrame
+def get_words_by_window(sentences_df,verbs_df,window_size=2):
+    df_list = []
+    for _, verb_row in verbs_df.iterrows():
+        verb = verb_row['text']
+        phrase_id = verb_row['phrase id']
+        sentence_row = sentences_df.loc[sentences_df['phrase id'] == phrase_id]
+        index = sentence_row['index'].values[0]
+        # print(index)
+        # Find all rows in sentences_df that have a target verb value equal to verb by masking
+        related_rows = sentences_df[combined_df['target verb'] == verb]
+        related_rows = related_rows["text"].tolist()
+        # print(related_rows)
+        window = get_window(related_rows,index,window_size=window_size)
+        window_df = pd.DataFrame({'word': window, 'target word': verb})
+        df_list.append(window_df)
+
+    result_df = pd.concat(df_list, ignore_index=True)
+    return result_df
+        # print(f'Verb: {verb}, phrase id: {phrase_id}, Related words: {related_rows["text"].tolist()}')
+
+window = 1
+window_df = get_words_by_window(combined_df,verbs_df,window)
+window_df.to_excel(f'window_words_{window}.xlsx', index=False)
+
