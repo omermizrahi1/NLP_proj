5815c361c5cabed9b72714ce67c894d8eb7696b2
omer <omer.mizrahi@hpe.com>
2023-08-02 09:25:36 +0300

    ran optuna on the data

--- model.py
+++ model.py
@@ -17,89 +17,6 @@ import optuna
 from optuna.samplers import TPESampler, CmaEsSampler
 from optuna import Trial, visualization
 
-
-
-def pca_analysis(X_encoded):
-    # Create the PCA instance and fit and transform the data with pca
-    pca = PCA()
-
-    # Fit the PCA model to your data
-    pca.fit(X_encoded)
-
-    # Get the explained variance ratio for each component
-    explained_variance_ratio = pca.explained_variance_ratio_
-
-    # Calculate the cumulative explained variance ratio
-    cumulative_explained_variance = np.cumsum(explained_variance_ratio)
-
-    # Plot the cumulative explained variance ratio
-    plt.plot(range(1, len(cumulative_explained_variance) + 1), cumulative_explained_variance, marker='o')
-    plt.xlabel('Number of Components')
-    plt.ylabel('Cumulative Explained Variance Ratio')
-    plt.show()
-
-    plt.plot(range(1, len(explained_variance_ratio) + 1), explained_variance_ratio, marker='o')
-    plt.xlabel('Number of Components')
-    plt.ylabel('Explained Variance Ratio')
-    plt.show()
-
-    # Set the threshold for the desired variance to retain
-    desired_variance = 0.95
-
-    # Find the number of components that achieve the desired variance
-    n_components = np.argmax(cumulative_explained_variance >= desired_variance) + 1
-    print("Number of components to retain", desired_variance * 100, "% variance:", n_components)
-
-methods = ['glinert', 'blau']
-
-
-for method in methods:
-    df = pd.read_excel(f'merged_{method}.xlsx')
-    df.replace({None: pd.NA}, inplace=True)
-    df.drop(columns=['target word_0'], inplace=True)
-    df.dropna(subset=[method.capitalize()], inplace=True)
-    df = df[df[method.capitalize()] != '-']
-    df.fillna('', inplace=True)
-
-    X = df.iloc[:, :-1]
-    X.to_excel('X.xlsx', index=False)
-    print(f"X size = {X.size}")  
-    y = df.iloc[:, -1]   
-    print(f"y size = {y.size}")
-
-    encoder = OneHotEncoder(sparse=False , handle_unknown='ignore')
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    X_train = encoder.fit_transform(X_train)
-    X_test = encoder.fit_transform(X_test)
-
-    label_encoder = LabelEncoder()
-    y_train = label_encoder.fit_transform(y_train)
-    y_test = label_encoder.fit_transform(y_test)
-
-    # clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
-    # models,predictions = clf.fit(X_train, X_test, y_train, y_test)
-    # print(models)
-
-
-
-# best models for glinert
-# NearestCentroid                    0.57               0.43    None      0.57        0.04
-# LGBMClassifier                     0.59               0.28    None      0.54        0.91
-# XGBClassifier                      0.59               0.28    None      0.54        1.21
-# ExtraTreesClassifier               0.58               0.28    None      0.53        0.30
-# RandomForestClassifier 
-
-
-# best models for blau
-# NearestCentroid                    0.56               0.37    None      0.56        0.04
-# XGBClassifier                      0.64               0.30    None      0.59        0.94
-# LGBMClassifier                     0.63               0.29    None      0.59        0.68
-# RandomForestClassifier             0.62               0.29    None      0.57        0.30
-# ExtraTreesClassifier               0.60               0.28    None      0.55        0.28
-
-
-
-
 def objective(trial):
 
     
@@ -161,11 +78,88 @@ def objective(trial):
     accuracy = score.mean()
     return accuracy
 
-# study = optuna.create_study(direction="maximize", sampler=TPESampler())
-# study.optimize(objective, n_trials=1000)
-# print('best_trial\n', study.best_trial)
-# print('best_params\n', study.best_params)
-# print('best_value\n', study.best_value)
+def pca_analysis(X_encoded):
+    # Create the PCA instance and fit and transform the data with pca
+    pca = PCA()
+
+    # Fit the PCA model to your data
+    pca.fit(X_encoded)
+
+    # Get the explained variance ratio for each component
+    explained_variance_ratio = pca.explained_variance_ratio_
+
+    # Calculate the cumulative explained variance ratio
+    cumulative_explained_variance = np.cumsum(explained_variance_ratio)
+
+    # Plot the cumulative explained variance ratio
+    plt.plot(range(1, len(cumulative_explained_variance) + 1), cumulative_explained_variance, marker='o')
+    plt.xlabel('Number of Components')
+    plt.ylabel('Cumulative Explained Variance Ratio')
+    plt.show()
+
+    plt.plot(range(1, len(explained_variance_ratio) + 1), explained_variance_ratio, marker='o')
+    plt.xlabel('Number of Components')
+    plt.ylabel('Explained Variance Ratio')
+    plt.show()
+
+    # Set the threshold for the desired variance to retain
+    desired_variance = 0.95
+
+    # Find the number of components that achieve the desired variance
+    n_components = np.argmax(cumulative_explained_variance >= desired_variance) + 1
+    print("Number of components to retain", desired_variance * 100, "% variance:", n_components)
+
+methods = ['glinert', 'blau']
+
+
+for method in methods:
+    df = pd.read_excel(f'merged_{method}.xlsx')
+    df.replace({None: pd.NA}, inplace=True)
+    df.drop(columns=['target word_0'], inplace=True)
+    df.dropna(subset=[method.capitalize()], inplace=True)
+    df = df[df[method.capitalize()] != '-']
+    df.fillna('', inplace=True)
+
+    X = df.iloc[:, :-1]
+    print(f"X size = {X.size}")  
+    y = df.iloc[:, -1]
+    print(f"y size = {y.size}")
+
+    encoder = OneHotEncoder(sparse=False , handle_unknown='ignore')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train = encoder.fit_transform(X_train)
+    X_test = encoder.fit_transform(X_test)
+
+    label_encoder = LabelEncoder()
+    y_train = label_encoder.fit_transform(y_train)
+    y_test = label_encoder.fit_transform(y_test)
+
+    # clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
+    # models,predictions = clf.fit(X_train, X_test, y_train, y_test)
+    # print(models)
+    study = optuna.create_study(direction="maximize", sampler=TPESampler())
+    study.optimize(objective, n_trials=1000)
+    file_path = f"optuna_{method}.txt"
+    try:
+        with open(file_path, 'w') as file:
+            file.write(f'best_trial: {study.best_trial}\nbest_params: {study.best_params}\nbest_value: {study.best_value}')
+        print("Data has been written to the file.")
+    except IOError as e:
+        print(f"An error occurred while writing to the file: {e}")
 
 
 
+# best models for glinert
+# NearestCentroid                    0.57               0.43    None      0.57        0.04
+# LGBMClassifier                     0.59               0.28    None      0.54        0.91
+# XGBClassifier                      0.59               0.28    None      0.54        1.21
+# ExtraTreesClassifier               0.58               0.28    None      0.53        0.30
+# RandomForestClassifier 
+
+
+# best models for blau
+# NearestCentroid                    0.56               0.37    None      0.56        0.04
+# XGBClassifier                      0.64               0.30    None      0.59        0.94
+# LGBMClassifier                     0.63               0.29    None      0.59        0.68
+# RandomForestClassifier             0.62               0.29    None      0.57        0.30
+# ExtraTreesClassifier               0.60               0.28    None      0.55        0.28
